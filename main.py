257304a4816b12bc7cb94b6e846cdf3d3fd23b8d
evelina-meijere <124715450+evelina-meijere@users.noreply.github.com>
2023-03-18 11:22:40 +0000

    pirmais

--- main.py
+++ main.py
@@ -2,12 +2,25 @@
 
 def parallel_processing(n, m, data):
     output = []
+    laiks = list(data)
+    thread = [0] * n 
+
+    for i in range(k):
+        nakamais = min(range(n), key = lambda x: thread[x])
+        out.append((nakamais, i))
+        thread[nakamais] += laiks[i]
+
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
 
     return output
 
 def main():
+    n, m = map(int,input().split())
+    data = list(map(int, input(). split()))
+    result = parallel_processing(n,m,data)
+    for pari in result:
+        print(pari[0],pari[1])
     # TODO: create input from keyboard
     # input consists of two lines
     # first line - n and m
@@ -21,7 +34,7 @@ def main():
     data = []
 
     # TODO: create the function
-    result = parallel_processing(n,m,data)
+    
     
     # TODO: print out the results, each pair in it's own line
 
