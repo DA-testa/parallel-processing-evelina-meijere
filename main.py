f9e53634dba4b3d87ce7558b06eb288d629d0ebf
evelina-meijere <124715450+evelina-meijere@users.noreply.github.com>
2023-03-18 11:53:15 +0000

    bbbbbbbbb

--- main.py
+++ main.py
@@ -7,13 +7,10 @@ def parallel_processing(n, m, data):
 
     for i in range(m):
         nakamais = thread.index(min(thread))
-        thread[nakamais] += laiks[i]
         output.append((nakamais, i))
+        thread[nakamais] += laiks[i]
         
-
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
-
+   
     return output
 
 def main():
@@ -25,24 +22,6 @@ def main():
             print(pari[0],pari[1])
     except ValueError:
         print("Kļūda ievadē")
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
-    #n = 0
-   # m = 0
-
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    #data = []
-
-    # TODO: create the function
-    
-    
-    # TODO: print out the results, each pair in it's own line
-
-
-
+  
 if __name__ == "__main__":
     main()
